1cc8eb51964fcb4a9ab0da53c1347525778203dd
Claudio Torcato <claudiotorcato@gmail.com>
2023-10-20 22:51:36 -0300

    Cadastro de decks

--- gehenna_api/models.py
+++ gehenna_api/models.py
@@ -72,8 +72,8 @@ class Deck(Base):
     player: Mapped[Optional[str]]
     tipo: Mapped[str]
     created: Mapped[datetime.date]
-    updated: Mapped[datetime.datetime]
-    preconstructed: Mapped[bool]
+    updated: Mapped[Optional[datetime.datetime]]
+    preconstructed: Mapped[bool] = mapped_column(default=False)
     owner_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     owner: Mapped['User'] = relationship()
     code: Mapped[int]   # codigo no gehenna legacy
--- gehenna_api/routes/decks.py
+++ gehenna_api/routes/decks.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import Session
 
 from gehenna_api.database import get_session
 from gehenna_api.models import Deck, User
-from gehenna_api.schemas import DeckList
+from gehenna_api.schemas import DeckList, DeckPublic, DeckSchema
 
 router = APIRouter(prefix='/decks', tags=['decks'])
 
 
 @router.get('/{username}', response_model=DeckList)
-def decks(
+def read_decks(
     username: str,
     skip: int = 0,
     limit: int = 100,
@@ -23,3 +23,18 @@ def decks(
         select(Deck).where(Deck.owner_id == 1).offset(skip).limit(limit)
     ).all()
     return {'decks': lista}
+
+@router.put('/{deck_id}', response_model=DeckPublic)
+def update_deck(
+        deck_id: int, 
+        deck: DeckSchema,
+        session: Session = Depends(get_session),
+    ):
+    db_deck = session.scalar(select(Deck).where(Deck.id == deck_id))
+    if db_deck is None:
+        raise HTTPException(status_code=404, detail='Deck not found')
+    db_deck.name = deck.name
+    db_deck.description = deck.description
+    session.commit()
+    session.refresh(db_deck)
+    return db_deck
